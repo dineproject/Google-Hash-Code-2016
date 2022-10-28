0551256165b4371b67ad7a3f351ae1e09275d9c0
Olivier Aubert <contact@olivieraubert.net>
2022-10-28 14:08:26 +0200

    Modify code

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Projet Poly#
 ============
 
-Une brève description ici.
+Décrire brièvement le projet ici.
 
-Le fichier `README.md` est écrit en **Markdown** et permet de soigner la _mise en forme_.
+Le fichier `README.md` est écrit en [**Markdown**](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax) et permet de soigner la _mise en forme_.
 
 Fonctionnement du projet
 ========================
@@ -14,7 +14,7 @@ Description du fonctionnement du projet, notamment comment le lancer.
 L'équipe
 ========
 
-à compléter: 1 ligne par personne, indiquant le nom et l'adresse mail de la personne
+à compléter par chaque personne: 1 ligne par personne, indiquant le nom et l'adresse mail de la personne
 
 - qui ?
 
--- polyhash.py
+++ polyhash.py
@@ -4,15 +4,16 @@
 """Module principal pour la mise en oeuvre du projet Poly#.
 """
 
-# Structurez votre code en modules pour améliorer la compréhension et
-# faciliter le travail collaboratif
+# Vous pouvez structurer votre code en modules pour améliorer la
+# compréhension et faciliter le travail collaboratif
 from parser import parse_challenge
 from solver import solve
+from scorer import score_solution
 
 if __name__ == "__main__":
     # On fournit ici un exemple permettant de passer un simple
     # argument (le fichier du challenge) en paramètre. N'hésitez pas à
-    # compléter avec d'autres paramètres.
+    # compléter avec d'autres paramètres/options.
     import argparse
     parser = argparse.ArgumentParser(description='Solve Poly# challenge.')
     parser.add_argument('challenge', type=str,
@@ -20,4 +21,7 @@ if __name__ == "__main__":
                         metavar="challenge.txt")
     args = parser.parse_args()
 
-    solve(parse_challenge(args.challenge))
+    challenge = parse_challenge(args.challenge)
+    solution = solve(challenge)
+    print(f"Score: {score_solution(solution)}")
+
